eff9d7c61e0b09708660482314b5338da4c9d78d
kimochiee <90102971+kimochiee@users.noreply.github.com>
2023-12-25 00:46:00 +0700

    update project structure

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 import theme from './theme'
 import CssBaseline from '@mui/material/CssBaseline'
-import GlobalStyles from '@mui/material/GlobalStyles'
+// import GlobalStyles from '@mui/material/GlobalStyles'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
